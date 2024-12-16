a53f49df0a8257c87b542cccc016fb151ec2d0f4
Rutwik Dadgal <91602615+rutwikdadgal@users.noreply.github.com>
2024-12-16 17:14:11 +0530

    Update storage_account.tf

--- storage_account.tf
+++ storage_account.tf
@@ -1,11 +1,11 @@
-# resource "random_string" "suffix" {
-#   length  = 6
-#   special = false
-#   upper = false
-# }
+ resource "random_string" "suffix" {
+   length  = 6
+   special = false
+   upper = false
+}
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "${var.Storage_account_name}"      #${random_string.suffix.result}
+  name                     = "${var.Storage_account_name}${random_string.suffix.result}"      
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = var.storage_account_account_tier
